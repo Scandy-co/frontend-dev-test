82eb5bcee95f917db0af269126a67c4284632f9e
Neilson Gamble <ngamble@redventures.com>
2019-05-06 21:16:47 -0400

    init commit. adding files from create-react-app starter, material-ui and react-dropzone

---
+++ scandy-dev-test/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'
+import ImageDropContainer from './components/ImageDropContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ImageDropContainer />
+    </div>
+  );
+}
+
+export default App;
