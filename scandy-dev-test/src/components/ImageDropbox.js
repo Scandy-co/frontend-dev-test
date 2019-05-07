081fca592ba00357b4de287c152277ce61eb0bb9
Neilson Gamble <ngamble@redventures.com>
2019-05-06 23:35:42 -0400

    adding firebase to project and coding out basic uploaded functionality.  need to add in progress indicator and then move on to cloud functions

--- scandy-dev-test/src/components/ImageDropbox.js
+++ scandy-dev-test/src/components/ImageDropbox.js
@@ -1,6 +1,22 @@
 import React, { useCallback, Fragment } from 'react'
 import { useDropzone } from 'react-dropzone'
-import RemoveButton from './RemoveButton';
+import UploadButton from './UploadButton';
+// import RemoveButton from './RemoveButton';
+import * as firebase from "firebase/app";
+import 'firebase/storage';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDGgR-Rr4P3FNWVPT8c1Gkk-0nF9XleAOw",
+    authDomain: "scandy-dev-test.firebaseapp.com",
+    databaseURL: "https://scandy-dev-test.firebaseio.com",
+    projectId: "scandy-dev-test",
+    storageBucket: "scandy-dev-test.appspot.com",
+    messagingSenderId: "857560367946",
+    appId: "1:857560367946:web:7a251a24d8670c7d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default function MyDropzone() {
 
@@ -48,15 +64,39 @@ export default function MyDropzone() {
     maxWidth: '100%',
   }
 
-  const handleRemove = function(e) {
-    e.stopPropagation();
-    document.getElementById("imgfile").value = null;
+  // const handleRemove = function(e) {
+  //   e.stopPropagation();
+  //   document.getElementById("imgfile").value = null;
+  // }
+
+  const handleUpload = function(e) {
+    console.log(acceptedFiles[0])
+
+    // Create a root reference
+    var storageRef = firebase.storage().ref();
+
+    // Create a reference to 'upload.jpg'
+    var uploadRef = storageRef.child('upload.jpg');
+
+    // Create a reference to 'images/upload.jpg'
+    var uploadImageRef = storageRef.child('images/upload.jpg');
+
+    // While the file names are the same, the references point to different files
+    // uploadRef.name === uploadImageRef.name            // true
+    // uploadRef.fullPath === uploadImageRef.fullPath    // false
+
+    var file = acceptedFiles[0]
+    uploadRef.put(file).then(function(snapshot) {
+      console.log('Uploaded a blob or file!');
+    });
+
+
   }
 
   const showDropBox = acceptedFiles.length === 0
-  console.log(showDropBox)
 
   return (
+    <Fragment>
     <div style={dropboxStyles} {...getRootProps()}>
       <form id="dropboxForm">
         <input id="imgfile" {...getInputProps()} />
@@ -71,10 +111,14 @@ export default function MyDropzone() {
           !showDropBox &&
           <Fragment>
             <canvas id="canvas" style={canvasStyles}></canvas>
-            <RemoveButton handleClick={handleRemove} />
+            {/* <RemoveButton handleClick={handleRemove} /> */}
           </Fragment>
         }
       </form>
     </div>
+      {
+        !showDropBox && <UploadButton handleClick={()=>handleUpload(acceptedFiles)}/>
+      }
+    </Fragment>
   )
 }
