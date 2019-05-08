261f2c1034e221376f9dcfdc86ad60ab23a27133
Neilson Gamble <ngamble@redventures.com>
2019-05-08 10:36:34 -0400

    adding in cloud function to email downloadlink to processed / checked image to email.  need to add configuration of email address

--- scandy-dev-test/functions/index.js
+++ scandy-dev-test/functions/index.js
@@ -1,10 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp()
 const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+admin.initializeApp()
 
 exports.checkImage = functions.storage.object().onFinalize(async (object) => {
   const fileBucket = object.bucket; // The Storage bucket that contains the file.
@@ -46,14 +47,75 @@ exports.checkImage = functions.storage.object().onFinalize(async (object) => {
     tempFilePath]);
 
   console.log('Checked image created at', tempFilePath);
+
   // We add a 'checked_' prefix to checked image file name. That's where we'll upload the checked image.
   const checkedFileName = `checked_${fileName}`;
   const checkedFilePath = path.join(path.dirname(filePath), checkedFileName);
+
   // Uploading the checked image.
   await bucket.upload(tempFilePath, {
     destination: checkedFilePath,
     metadata: metadata,
   });
+
   // Once the checked image has been uploaded delete the local file to free up disk space.
   return fs.unlinkSync(tempFilePath);
+});
+
+exports.sendCheckedImage = functions.storage.object().onFinalize(async (object) => {
+
+  const defaultStorage = admin.storage();
+  const bucket = defaultStorage.bucket();
+  const file = bucket.file(object.name);
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+  const fileName = path.basename(filePath); // Get the file name.
+
+  // options for getSignedUrl
+  const options = {
+    action: 'read',
+    expires: '03-17-2025'
+  };
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('image/')) {
+    return console.log('This is not an image.');
+  }
+
+  // Exit if this is triggered on the original file
+  if(!fileName.startsWith('checked_')) {
+    return console.log('This is not the checked image');
+  }  
+
+  // Get a signed URL for the file and email link to configured email
+  return file
+    .getSignedUrl(options)
+    .then(results => {
+      const url = results[0];
+      console.log(`The signed url for is ${url}.`);
+
+      const gmailEmail = functions.config().gmail.email;
+      const gmailPassword = functions.config().gmail.password;
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: gmailEmail,
+          pass: gmailPassword,
+        },
+      });
+
+      const mailOptions = {
+        from: 'neilgamb@gmail.com',
+        to: 'neilgamb@gmail.com',
+        subject: 'Image Drop Results!',
+        text: `Please see the link to download your image: ${url}`
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
+        console.log('Message sent!', err);
+      });
+
+      return true;
+    })
+
 });
